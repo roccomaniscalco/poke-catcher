439659196fb548f87b5dd1ecaa2444d8a3e2c60c
Rocco <roccomaniscalco2001@gmail.com>
2021-07-10 15:58:38 -0400

    built discord bot to detect messages and reply

--- pokeCatcher.js
+++ pokeCatcher.js
@@ -1,1 +1,19 @@
-console.log("hey");
+require('dotenv').config();
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (
+    message.embeds[0] &&
+    message.embeds[0].title === "A wild pokémon has appeared!"
+  ) {
+    message.channel.send("Catch!");
+  }
+});
+
+client.login(process.env.BOT_KEY);
