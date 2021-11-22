0937605c73bd4e45100e26dcbebeb81e2cfb4f49
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2021-11-22 04:49:35 -0500

    added documentation and improved readability

--- pokeCatcher.js
+++ pokeCatcher.js
@@ -1,19 +1,24 @@
-require('dotenv').config();
-const Discord = require("discord.js");
+const { Client } = require("discord.js");
+require("dotenv").config();
 
-const client = new Discord.Client();
+// create a Client instance (bot) to interact with the Discord server
+const bot = new Client();
 
-client.on("ready", () => {
-  console.log("I am ready!");
+bot.on("ready", () => {
+  // log in green then switches to white for subsequent logs
+  console.log("\x1b[32m", "• PokéCatcher Active •", "\x1b[0m");
 });
 
-client.on("message", (message) => {
-  if (
-    message.embeds[0] &&
-    message.embeds[0].title === "A wild pokémon has appeared!"
-  ) {
-    message.channel.send("Catch!");
+bot.on("message", (message) => {
+  // if a message embed title is "A wild pokémon has appeared!"
+  if (message.embeds[0]?.title === "A wild pokémon has appeared!") {
+    message.channel.send(message.embeds[0].image.url);
+  }
+  // if a message content is "ping"
+  if (message.content === "ping") {
+    message.channel.send("pong");
   }
 });
 
-client.login(process.env.BOT_KEY);
+// establish connection to the Discord server via a bot key
+bot.login(process.env.BOT_KEY);
